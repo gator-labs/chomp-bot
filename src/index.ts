ade88af9964ea6130cc5bd9f680bb281e21573d4
Neel <neelp0980@gmail.com>
2024-08-26 19:06:50 +0530

    feat: user action state

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Telegraf, Markup, Context } from 'telegraf';
+import { Telegraf, Markup } from 'telegraf';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
 import { kv } from '@vercel/kv';
@@ -39,7 +39,7 @@ bot.on('inline_query', (ctx) => {
   });
 });
 
-const replyWithPrimaryOptions = async (ctx: Context) => {
+const replyWithPrimaryOptions = async (ctx: any) => {
   const prompt = 'What do you want to do today?';
   const buttonOptions: { [k: string]: string } = {
     'new.quickstart': 'Answer questions 🎲',
@@ -48,6 +48,7 @@ const replyWithPrimaryOptions = async (ctx: Context) => {
   const buttons = Object.keys(buttonOptions).map((key) =>
     Markup.button.callback(buttonOptions[key], key),
   );
+  await kv.set(`state:${ctx?.from?.id}`, 'INIT');
   ctx.reply(prompt, Markup.inlineKeyboard(buttons));
 };
 
@@ -80,33 +81,37 @@ bot.start(async (ctx) => {
   NEW -> ANSWERING FIRST ORDER
 */
 bot.action('new.quickstart', async (ctx) => {
-  const user = (await kv.get(`user:${ctx.from.id}`)) as IChompUser;
-  const questionDeck = await getQuestion(user.id);
+  const state = await kv.get(`state:${ctx.from.id}`);
+  if (state === 'INIT') {
+    const user = (await kv.get(`user:${ctx.from.id}`)) as IChompUser;
+    const questionDeck = await getQuestion(user.id);
 
-  if (questionDeck) {
-    await kv.set(`question:${ctx.from.id}`, questionDeck);
-    const { question, questionOptions, imageUrl } = questionDeck;
-    const prompt = question;
+    if (questionDeck) {
+      await kv.set(`question:${ctx.from.id}`, questionDeck);
+      const { question, questionOptions, imageUrl } = questionDeck;
+      const prompt = question;
 
-    const buttons = questionOptions.map(
-      (option: { id: number; option: string; isLeft: boolean }) =>
-        Markup.button.callback(
-          `${option.option}`,
-          `answering-first-order.${option.id}`,
-        ),
-    );
-    if (imageUrl) {
-      ctx.replyWithPhoto(
-        { url: imageUrl },
-        { caption: prompt, ...Markup.inlineKeyboard(buttons) },
+      const buttons = questionOptions.map(
+        (option: { id: number; option: string; isLeft: boolean }) =>
+          Markup.button.callback(
+            `${option.option}`,
+            `answering-first-order.${option.id}`,
+          ),
       );
+      await kv.set(`state:${ctx.from.id}`, 'FIRST');
+      if (imageUrl) {
+        ctx.replyWithPhoto(
+          { url: imageUrl },
+          { caption: prompt, ...Markup.inlineKeyboard(buttons) },
+        );
+      } else {
+        ctx.reply(prompt, Markup.inlineKeyboard(buttons));
+      }
     } else {
-      ctx.reply(prompt, Markup.inlineKeyboard(buttons));
+      ctx.reply(
+        'You have already chomp all questions. Please visit later to chomp it.',
+      );
     }
-  } else {
-    ctx.reply(
-      'You have already chomp all questions. Please visit later to chomp it.',
-    );
   }
 });
 
@@ -114,78 +119,93 @@ bot.action('new.quickstart', async (ctx) => {
   ANSWERING SECOND ORDER
 */
 bot.action(/^answering-first-order\.(.+)$/, async (ctx) => {
-  const questionDeck = (await kv.get(`question:${ctx.from.id}`)) as IQuestion;
-  const userAnswerId = parseInt(ctx.match[1], 10);
-  const userAnswer = questionDeck?.questionOptions.find(
-    (option) => option.id === userAnswerId,
-  )?.option;
+  const state = await kv.get(`state:${ctx.from.id}`);
+  if (state === 'FIRST') {
+    const questionDeck = (await kv.get(`question:${ctx.from.id}`)) as IQuestion;
+    const userAnswerId = parseInt(ctx.match[1], 10);
+    const userAnswer = questionDeck?.questionOptions.find(
+      (option) => option.id === userAnswerId,
+    )?.option;
 
-  const secondPrompt = `What percentage of people do you think answered ${userAnswer}?`;
+    const secondPrompt = `What percentage of people do you think answered ${userAnswer}?`;
 
-  const secondButtonOptions: { [k: string]: string } = {
-    'answering-second-order.0': '0%',
-    'answering-second-order.10': '10%',
-    'answering-second-order.20': '20%',
-    'answering-second-order.30': '30%',
-    'answering-second-order.40': '40%',
-    'answering-second-order.50': '50%',
-    'answering-second-order.60': '60%',
-    'answering-second-order.70': '70%',
-    'answering-second-order.80': '80%',
-    'answering-second-order.90': '90%',
-    'answering-second-order.100': '100%',
-  };
+    const secondButtonOptions: { [k: string]: string } = {
+      'answering-second-order.0': '0%',
+      'answering-second-order.10': '10%',
+      'answering-second-order.20': '20%',
+      'answering-second-order.30': '30%',
+      'answering-second-order.40': '40%',
+      'answering-second-order.50': '50%',
+      'answering-second-order.60': '60%',
+      'answering-second-order.70': '70%',
+      'answering-second-order.80': '80%',
+      'answering-second-order.90': '90%',
+      'answering-second-order.100': '100%',
+    };
 
-  const secondButtons = Object.keys(secondButtonOptions).map((key) =>
-    Markup.button.callback(secondButtonOptions[key], `${key}.${userAnswerId}`),
-  );
+    const secondButtons = Object.keys(secondButtonOptions).map((key) =>
+      Markup.button.callback(
+        secondButtonOptions[key],
+        `${key}.${userAnswerId}`,
+      ),
+    );
 
-  const formattedSecondButtons = [
-    secondButtons.slice(0, 5),
-    secondButtons.slice(5, 6),
-    secondButtons.slice(6),
-  ];
-
-  ctx.reply(secondPrompt, Markup.inlineKeyboard(formattedSecondButtons));
+    const formattedSecondButtons = [
+      secondButtons.slice(0, 5),
+      secondButtons.slice(5, 6),
+      secondButtons.slice(6),
+    ];
+    await kv.set(`state:${ctx.from.id}`, 'SECOND');
+    ctx.reply(secondPrompt, Markup.inlineKeyboard(formattedSecondButtons));
+  }
 });
 
 /*
   SAVING ANSWER
 */
 bot.action(/^answering-second-order\.(.+)\.(.+)$/, async (ctx) => {
-  const questionDeck = (await kv.get(`question:${ctx.from.id}`)) as IQuestion;
-  const user = (await kv.get(`user:${ctx.from.id}`)) as IChompUser;
-  const percentageGiven = Number(ctx.match[1]);
-  const questionOptionId = parseInt(ctx.match[2], 10);
+  const state = await kv.get(`state:${ctx.from.id}`);
+  if (state === 'SECOND') {
+    const questionDeck = (await kv.get(`question:${ctx.from.id}`)) as IQuestion;
+    const user = (await kv.get(`user:${ctx.from.id}`)) as IChompUser;
+    const percentageGiven = Number(ctx.match[1]);
+    const questionOptionId = parseInt(ctx.match[2], 10);
 
-  if (questionDeck) {
-    const { id: questionId, deckId } = questionDeck;
-    const { id: userId } = user;
+    if (questionDeck) {
+      const { id: questionId, deckId } = questionDeck;
+      const { id: userId } = user;
 
-    if (deckId) {
-      await saveDeck(
-        deckId,
-        userId,
-        questionId,
-        questionOptionId,
-        percentageGiven,
-      );
-    } else {
-      await saveQuestion(userId, questionId, questionOptionId, percentageGiven);
+      if (deckId) {
+        await saveDeck(
+          deckId,
+          userId,
+          questionId,
+          questionOptionId,
+          percentageGiven,
+        );
+      } else {
+        await saveQuestion(
+          userId,
+          questionId,
+          questionOptionId,
+          percentageGiven,
+        );
+      }
     }
+
+    const prompt =
+      'Well done! You just chomped a question.\n\nWhat do you want to do next?';
+    const buttonOptions: { [k: string]: string } = {
+      'new.quickstart': 'Answer more 🎲',
+      'completed-answering.home': 'Go home 🏡',
+    };
+
+    const buttons = Object.keys(buttonOptions).map((key) =>
+      Markup.button.callback(buttonOptions[key], key),
+    );
+    await kv.set(`state:${ctx.from.id}`, 'INIT');
+    ctx.reply(prompt, Markup.inlineKeyboard(buttons));
   }
-
-  const prompt =
-    'Well done! You just chomped a question.\n\nWhat do you want to do next?';
-  const buttonOptions: { [k: string]: string } = {
-    'new.quickstart': 'Answer more 🎲',
-    'completed-answering.home': 'Go home 🏡',
-  };
-
-  const buttons = Object.keys(buttonOptions).map((key) =>
-    Markup.button.callback(buttonOptions[key], key),
-  );
-  ctx.reply(prompt, Markup.inlineKeyboard(buttons));
 });
 
 bot.action('completed-answering.home', async (ctx) => {
@@ -197,31 +217,36 @@ bot.action('completed-answering.home', async (ctx) => {
 */
 
 bot.action('new.reveal', async (ctx) => {
-  const user = (await kv.get(`user:${ctx?.from?.id}`)) as IChompUser;
-  const questionRevealCount = await getRevealQuestion(user?.id);
+  const state = await kv.get(`state:${ctx.from.id}`);
+  if (state === 'INIT') {
+    const user = (await kv.get(`user:${ctx?.from?.id}`)) as IChompUser;
+    const questionRevealCount = await getRevealQuestion(user?.id);
 
-  if (!questionRevealCount) {
-    const prompt = `You don't have any questions to reveal. Answer more questions in order to reveal"`;
+    if (!questionRevealCount) {
+      const prompt = `You don't have any questions to reveal. Answer more questions in order to reveal"`;
 
-    const buttonOptions: { [k: string]: string } = {
-      'new.quickstart': 'Keep Chompin',
-    };
-    const buttons = Object.keys(buttonOptions).map((key) =>
-      Markup.button.callback(buttonOptions[key], key),
-    );
-    ctx.reply(prompt, Markup.inlineKeyboard(buttons));
-  } else {
-    const prompt = `You have ${questionRevealCount} questions available to reveal. Launch to Reveal?`;
+      const buttonOptions: { [k: string]: string } = {
+        'new.quickstart': 'Keep Chompin',
+      };
+      const buttons = Object.keys(buttonOptions).map((key) =>
+        Markup.button.callback(buttonOptions[key], key),
+      );
+      await kv.set(`state:${ctx?.from?.id}`, 'INIT');
+      ctx.reply(prompt, Markup.inlineKeyboard(buttons));
+    } else {
+      const prompt = `You have ${questionRevealCount} questions available to reveal. Launch to Reveal?`;
 
-    const buttonOptions: { [k: string]: string } = {
-      'selected-reveal.no': 'Maybe Later',
-      'selected-reveal.yes': 'Yes',
-    };
+      const buttonOptions: { [k: string]: string } = {
+        'selected-reveal.no': 'Maybe Later',
+        'selected-reveal.yes': 'Yes',
+      };
 
-    const buttons = Object.keys(buttonOptions).map((key) =>
-      Markup.button.callback(buttonOptions[key], key),
-    );
-    ctx.reply(prompt, Markup.inlineKeyboard(buttons));
+      const buttons = Object.keys(buttonOptions).map((key) =>
+        Markup.button.callback(buttonOptions[key], key),
+      );
+      await kv.set(`state:${ctx?.from?.id}`, 'REVEAL');
+      ctx.reply(prompt, Markup.inlineKeyboard(buttons));
+    }
   }
 });
 
@@ -233,12 +258,15 @@ bot.action('selected-reveal.no', async (ctx) => {
 });
 
 bot.action('selected-reveal.yes', async (ctx) => {
-  ctx.reply(
-    'Follow the link to burn BONK and reveal!',
-    Markup.inlineKeyboard([
-      Markup.button.webApp('Launch', WEB_APP_URL + '/bot'),
-    ]),
-  );
+  const state = await kv.get(`state:${ctx.from.id}`);
+  if (state === 'REVEAL') {
+    ctx.reply(
+      'Follow the link to burn BONK and reveal!',
+      Markup.inlineKeyboard([
+        Markup.button.webApp('Launch', WEB_APP_URL + '/bot'),
+      ]),
+    );
+  }
 });
 
 bot.on('message', async (ctx) => {
